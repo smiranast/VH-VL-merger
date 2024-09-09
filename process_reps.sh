7a2c29a86ca6a998cbbfc84c1c52ce3fb17a8046
smiranast <30687234+smiranast@users.noreply.github.com>
2024-09-09 11:50:56 +0300

    Add files via upload

--- process_reps.sh
+++ process_reps.sh
@@ -4,7 +4,7 @@ input_dir="" # Directory with input fastq files for amplicone 1
 while read s; do
         echo "$s"
 	cutadapt -g CAGRTBCAGCTKGTRCAGTCTGG -g CAGRTCACCTTGARGGAGTCTGGTCCT -g SARGTGCAKCTGGTGGAGTC -g CAGSTRCAGCTRCAGSAGTC -G TGARGAGACGGTGACCRTKGTCCC -G TGAGGAGACRGTGACCAGGGT -o $s"_R1_001.trimmed.fastq" -p $s"_R2_001.trimmed.fastq" $input_dir/$s"_R1_001.fastq.gz" $input_dir/$s"_R2_001.fastq.gz" -j 3 --discard-untrimmed --action=retain
-        mixcr -Xmx20G analyze amplicon -s hs --starting-material dna -t 4 --contig-assembly --5-end v-primers --3-end j-primers --adapters adapters-present --align -OvParameters.parameters.absoluteMinScore=40 --align -OdParameters.absoluteMinScore=25 --align -OjParameters.parameters.absoluteMinScore=40 --align -OmaxHits=3 --library my_library_IGH.json -r report_$s"_2" $s"_R1_001.trimmed.fastq" $s"_R2_001.trimmed.fastq"  $s"_2" --receptor-type igh --export "--preset min" --export "-cloneId -fraction -targetSequences"
+    mixcr -Xmx20G analyze generic-amplicon --species hsa --dna --floating-left-alignment-boundary --floating-right-alignment-boundary J -t 4 --library my_library_IGH.json --assemble-contigs-by CDR3 --add-step assembleContigs --append-export-clones-field -vHit --append-export-clones-field -dHit --append-export-clones-field -jHit --append-export-clones-field -cHit -f $s"_R1_001.trimmed.fastq" $s"_R2_001.trimmed.fastq"  $s"_2"
 done < "$sample_file"
 
 
@@ -15,9 +15,16 @@ while read s; do
         echo "$s"
        cutadapt -G TGGAGCTGAGCAGCCTGAGATCTGA -G CAATGACCAACATGGACCCTGTGG -G TCTGCAAATGAACAGCCTGAGAGCC -G AGCTCTGTGACCGCCGCGGACAC -G CACCGCCTACCTGCAGTGGAGC -G TTCTCCCTGCAGCTGAACTCTGTG -G CACGGCATATCTGCAGATCA -G Tggagctgagcagcctgagatccga -G Tctgcaaatgaacagcctgaaaacc -G TTctgcaaatgaacagtctgagaRct -G Ttcttcaaatgggcagcctgagagct -G cacggcatatctgcagatct -G TAgctctgtgactgccgcagacac -o $s"_R1_001.trimmed.fastq" -p $s"_R2_001.trimmed.fastq" $input_dir/$s"_R1_001.fastq.gz" $input_dir/$s"_R2_001.fastq.gz" -j 3 --discard-untrimmed --action=retain
 
-       mixcr -Xmx20G analyze amplicon -s hs --starting-material dna -t 4 --contig-assembly --5-end v-primers --3-end j-primers --adapters adapters-present --align -OvParameters.parameters.absoluteMinScore=40 --align -OdParameters.absoluteMinScore=25 --align -OjParameters.parameters.absoluteMinScore=40 --align -OmaxHits=3 --library mylibrary_IGKL.json -r report_$s"_IGKL" $s"_R1_001.trimmed.fastq"  $s"_IGKL" --receptor-type bcr --export "--preset min" --export "-cloneId -fraction -targetSequences"
-        mixcr exportAlignments --preset min -cloneId -readIds -chains -targetSequences $s"_IGKL".clna $s"_IGKL".alignments.txt -f
+       mixcr -Xmx20G analyze generic-amplicon --species hsa --dna --floating-left-alignment-boundary --floating-right-alignment-boundary J -t 4 --library mylibrary_IGKL.json --assemble-contigs-by CDR3 --add-step assembleContigs --append-export-clones-field -vHit --append-export-clones-field -dHit --append-export-clones-field -jHit --append-export-clones-field -cHit -f  $s"_IGKL"
+       
+       # If there was no IGL chains
+       if [ ! -f $s""_IGKL.clones_IGL.tsv ]; then
+        head -n 1 $s""_IGKL.clones_IGK.tsv > $s""_IGKL.clones_IGL.tsv
+       fi
 
-	mixcr -Xmx20G analyze amplicon -s hs --starting-material dna -t 4 --contig-assembly --5-end v-primers --3-end j-primers --adapters adapters-present --align -OvParameters.parameters.absoluteMinScore=40 --align -OdParameters.absoluteMinScore=25 --align -OjParameters.parameters.absoluteMinScore=40 --align -OmaxHits=3 --library my_library_IGH.json -r report_$s"_IGH" $s"_R2_001.trimmed.fastq"  $s"_IGH" --receptor-type igh
-        mixcr exportAlignments --preset min -cloneId -readIds $s"_IGH".clna $s"_IGH".alignments.txt -f
+       
+       mixcr exportAlignments -vHit -dHit -jHit -cHit -cloneId -readIds -chains -targetSequences $s"_IGKL".clna $s"_IGKL".alignments.tsv -f
+
+       mixcr -Xmx20G analyze generic-amplicon --species hsa --dna --floating-left-alignment-boundary --floating-right-alignment-boundary J -t 4 --library my_library_IGH.json --assemble-contigs-by CDR3 --add-step assembleContigs  --append-export-clones-field -vHit --append-export-clones-field -dHit --append-export-clones-field -jHit --append-export-clones-field -cHit -f $s"_R2_001.trimmed.fastq" $s"_IGH"
+       mixcr exportAlignments -vHit -dHit -jHit -cHit -cloneId -readIds $s"_IGH".clna $s"_IGH".alignments.tsv -f
 done < "$sample_file"
